c80d461c04f83fd8d71382f4f97ee65b230da701
RC <ryan.mercado@tooltwist.com>
2023-08-15 15:25:08 +0800

    Initial commit

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,10 +1,13 @@
 const App = () => {
+  const loginServiceAPIKey = "API1EFFST6IE2ZZKU8L5TFSN1LFJR";
   const loginServiceParams = new URLSearchParams({
+    // protocol = https:// or http://
+    // host = domain (localhost:5173)
     success: `${window.location.protocol}${window.location.host}/login`,
     fail: `${window.location.protocol}${window.location.host}/unauthorized`,
   });
 
-  const loginServiceURL = `https://uat.tooltwist.io/loginservice/v2/oauth2/initiate/API1EFFST6IE2ZZKU8L5TFSN1LFJR/google?${loginServiceParams}`;
+  const loginServiceURL = `https://uat.tooltwist.io/loginservice/v2/oauth2/initiate/${loginServiceAPIKey}/google?${loginServiceParams}`;
 
   const handleLogin = () => {
     window.location.href = loginServiceURL;
